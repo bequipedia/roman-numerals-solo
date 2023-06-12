76af8286846ac75506a5e25a36440c330dc04669
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-12 17:59:42 +0200

    pass failing test using tpp conditional

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,15 +1,16 @@
 public class RomanNumerals {
 
-    public static final String I = "I";
     public static final String V = "V";
+    public static final String I = "I";
 
     public String convert(int inputNumber) {
         String romanNumeral = "";
+        if(inputNumber >= 5) {
+            romanNumeral = V;
+            inputNumber -= 5;
+        }
         for(int i = 0; i < inputNumber; i++) {
             romanNumeral += I;
-        }
-        if(inputNumber == 5) {
-            romanNumeral = V;
         }
         return romanNumeral;
     }
