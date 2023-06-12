4d273a68a91b573f1c10b2eb1301bf5ea80ecd5b
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-12 12:42:36 +0200

    pass failing test

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,5 +1,8 @@
 public class RomanNumerals {
     public String convert(int inputNumber) {
+        if(inputNumber == 3) {
+            return "III";
+        }
         if(inputNumber == 2) {
             return "II";
         }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.TimeUnit;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
