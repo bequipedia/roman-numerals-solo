f1a509f2c16a65a215c1c4a02118f1f2f75f85d9
Alasdair Smith <alasdairsmith0491@gmail.com>
2023-06-12 11:41:12 +0100

    Add failing test for 3

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,5 +1,8 @@
 public class RomanNumerals {
     public String convert(int inputNumber) {
+        if(inputNumber == 2) {
+            return "II";
+        }
         return "I";
     }
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomanNumeralsTest {
 
     @Test
-    void should_return_1_when_I_given() {
+    void should_return_I_when_1_given() {
         RomanNumerals romanNumerals = new RomanNumerals();
         String expectedOutput = "I";
         int inputNumber = 1;
@@ -17,10 +17,21 @@ public class RomanNumeralsTest {
         assertEquals(expectedOutput, actualOutput);
     }
     @Test
-    void should_return_2_when_II_given() {
+    void should_return_II_when_2_given() {
         RomanNumerals romanNumerals = new RomanNumerals();
         String expectedOutput = "II";
         int inputNumber = 2;
+
+        String actualOutput = romanNumerals.convert(inputNumber);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void should_return_III_when_3_given() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        String expectedOutput = "III";
+        int inputNumber = 3;
 
         String actualOutput = romanNumerals.convert(inputNumber);
 
